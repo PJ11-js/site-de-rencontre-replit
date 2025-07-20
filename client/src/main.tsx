efac4220f0e6991f130935c0b8723cc9a7b50018
PJ11-js <pierrengendakuriyo5@gmail.com>
2025-07-20 20:59:29 +0200

    Create main.tsx

---
+++ client/src/main.tsx
@@ -0,0 +1,7 @@
+// client/src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => <h1>Bienvenue sur mon site de rencontre !</h1>;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
